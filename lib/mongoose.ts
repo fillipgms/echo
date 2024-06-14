60f10e21a12d9f7f0b1f140f3ac6282c5a42ec38
Fillip Mangia <fillipgms@gmail.com>
2024-06-14 14:01:00 -0300

    mongoose ajust

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -5,15 +5,18 @@ let isConnected = false;
 export const connectToDB = async () => {
     mongoose.set("strictQuery", true);
 
-    if (!process.env.MONGODB_URL)
-        return console.log("couldnt find mongo db url");
-
-    if (isConnected) return console.log("is already connected");
+    if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
+    if (isConnected) {
+        console.log("MongoDB connection already established");
+        return;
+    }
 
     try {
+        console.log("Tentando conexão");
         await mongoose.connect(process.env.MONGODB_URL);
-        isConnected = true;
-        console.log("connected to mongo db");
+
+        isConnected = true; // Set the connection status to true
+        console.log("MongoDB connected");
     } catch (error) {
         console.log(error);
     }
