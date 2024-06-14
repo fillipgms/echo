0adc7b13867a3a1b1bffbad05133495c80ed2f8f
Fillip Mangia <fillipgms@gmail.com>
2024-06-14 12:40:02 -0300

    second step

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,8 +1,26 @@
 import { headers } from "next/headers";
 import { Svix } from "svix";
 
-export async function POST(req: Request) {
-    const reqString = JSON.stringify(req);
+export async function POST(request: Request) {
+    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-    return new Response(`Received req: ${reqString}`, { status: 200 });
+    if (!WEBHOOK_SECRET) {
+        return new Response(`No webhook`, { status: 300 });
+    }
+
+    const payload = await request.json();
+    const header = headers();
+
+    if (payload && header) {
+        console.log("payload", payload);
+        console.log("header", payload);
+    }
+
+    const heads = {
+        "svix-id": header.get("svix-id"),
+        "svix-timestamp": header.get("svix-timestamp"),
+        "svix-signature": header.get("svix-signature"),
+    };
+
+    return new Response(`Received payload: ${payload}`, { status: 200 });
 }
